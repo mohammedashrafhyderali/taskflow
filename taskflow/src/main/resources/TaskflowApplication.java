71cd26b59f6d21d577e7dab77e30d8583a8195f4
Mohammed Ashraf Hyderali <mohammedashrafibnhyderali@gmail.com>
2026-04-07 16:00:55 +0530

    Remove misplaced Java file from resources

--- taskflow/src/main/resources/TaskflowApplication.java
+++
@@ -1,9 +0,0 @@
-package com.taskflow.taskflow;
-
-@SpringBootApplication
-public class TaskflowApplication {
-    public static void main(String[] args) {
-
-        SpringApplication.run(TaskflowApplication.class, args);
-    }
-}
